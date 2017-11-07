c93c66ccb83d8bb307f8b3ac77ed51794526d1c8
MajedDalain <majed-dalain@hotmail.com>
2017-11-07 21:09:02 +0100

    Add files via upload

---
+++ person.java
@@ -0,0 +1,51 @@
+public abstract class person {
+    private String name;
+    private String DOB;
+    private String address;
+    private String ID;
+
+
+
+    public person(){
+        this.name = "user Name";
+        this.ID = " user ID";
+        this.address ="user Address";
+        this.DOB = "..../../..";
+    }
+
+
+    public person(String name, String DOB, String adress, String ID) throws Exception {
+        if(name.equals("")){
+            throw new Exception(" An empty name, pleas Enter a valid name");
+        }
+        else{
+            this.name = name;
+            this.DOB = DOB;
+            this.address = adress;
+            this.ID = ID;
+
+        }
+
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+
+
+
+}
