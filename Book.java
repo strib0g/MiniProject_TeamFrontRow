45a426e2a0e40537e433db7086de26eb19ab1c53
Anotinas <guslindaaq@student.gu.se>
2017-11-15 13:03:57 +0100

    Added returnDate and a method in library that finds and returns it.(Will require bugtesting)

--- Book.java
+++ Book.java
@@ -7,9 +7,6 @@ public class Book implements Comparable<Book> {
     private String genre;
     private String publisher;
     private String shelf;
-    private int returnDay; // D
-    private int returnMonth; // D
-    private int returnYear; // D
     private boolean status;
 
 
@@ -23,7 +20,6 @@ public class Book implements Comparable<Book> {
         this.genre = genre;
         this.publisher = publisher;
         this.shelf = shelf;
-        this.returnDate = ""; //D
         this.status = true;
 
     }
@@ -56,18 +52,6 @@ public class Book implements Comparable<Book> {
     public String getTitle() {
         return title;
     }
-
-    public int getReturnDay(){
-        return returnDate;
-    } // D
-
-    public int getReturnMonth() {
-        return returnMonth;
-    } //D
-
-    public int getReturnYear() {
-        return returnYear;
-    } //D
 
     public boolean isStatus() {
         return status;
@@ -106,18 +90,6 @@ public class Book implements Comparable<Book> {
     public void setStatus(boolean status) {
         this.status = status;
     }
-
-    public void setReturnDay(int returnDay){
-        this.returnDay = returnDay;
-    } //D
-
-    public void setReturnMonth(int returnMonth) {
-        this.returnMonth = returnMonth;
-    } //D
-
-    public void setReturnYear(int returnYear) {
-        this.returnYear = returnYear;
-    } // D
 
     @Override
     public String toString() {
